f3d8b7e546bd3c592198f4f856d5df6308e1bcd9
Joseph Edusei <jedusei99@gmail.com>
2022-04-28 19:29:10 +0000

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace SplashScreenSample
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AndroidX.Core.SplashScreen.SplashScreen.InstallSplashScreen(this);
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            // Simulate delay to keep the splashscreen visible for a bit longer...
+            // Please note that you should *NEVER* do it this way in a production app,
+            // You can use a pre-draw listener instead: https://developer.android.com/guide/topics/ui/splash-screen#suspend-drawing
+            System.Threading.Thread.Sleep(5000);
+
+            // Set our view from the "main" layout resource 
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
